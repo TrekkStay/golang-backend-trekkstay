836f5f41e029af201bdd8b5d2bb8b57c97389c89
Phan Thanh An <thanhanphan17@gmail.com>
2024-01-19 16:11:10 +0700

    fix: validate phone existed and log error

--- modules/user/domain/usecase/create_user_usecase.go
+++ modules/user/domain/usecase/create_user_usecase.go
@@ -2,8 +2,10 @@ package usecase
 
 import (
 	"context"
+	"log/slog"
 	"trekkstay/modules/user/constant"
 	"trekkstay/modules/user/domain/entity"
+	"trekkstay/pkgs/log"
 	"trekkstay/utils"
 )
 
@@ -31,12 +33,33 @@ func (useCase createUserUseCaseImpl) ExecCreateUser(ctx context.Context, userEnt
 		"email": userEntity.Email,
 	})
 	if user != nil {
+		log.JsonLogger.Error("ExecCreateEmp.email_already_exists",
+			slog.String("request_id", ctx.Value("X-Request-ID").(string)),
+		)
+
 		return constant.ErrorEmailAlreadyExists(err)
+	}
+
+	// Check if phone already exists
+	user, err = useCase.readerRepo.FindUserByCondition(ctx, map[string]interface{}{
+		"phone": userEntity.Phone,
+	})
+	if user != nil {
+		log.JsonLogger.Error("ExecCreateEmp.phone_already_exists",
+			slog.String("request_id", ctx.Value("X-Request-ID").(string)),
+		)
+
+		return constant.ErrorPhoneAlreadyExists(err)
 	}
 
 	// Hash password
 	hashedPassword, err := utils.HashAndSalt([]byte(userEntity.Password))
 	if err != nil {
+		log.JsonLogger.Error("ExecCreateEmp.hash_password",
+			slog.Any("error", err.Error()),
+			slog.String("request_id", ctx.Value("X-Request-ID").(string)),
+		)
+
 		return constant.ErrorInternalServerError(err)
 	}
 	userEntity.Password = hashedPassword
@@ -44,6 +67,11 @@ func (useCase createUserUseCaseImpl) ExecCreateUser(ctx context.Context, userEnt
 	// Insert user
 	err = useCase.writerRepo.InsertUser(ctx, userEntity)
 	if err != nil {
+		log.JsonLogger.Error("ExecCreateEmp.insert_user",
+			slog.Any("error", err.Error()),
+			slog.String("request_id", ctx.Value("X-Request-ID").(string)),
+		)
+
 		return constant.ErrorInternalServerError(err)
 	}
 
