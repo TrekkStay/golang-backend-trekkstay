0818230c84214f60717c6c3ece68498c3a16d807
Phan Thanh An <thanhanphan17@gmail.com>
2024-01-20 01:24:48 +0700

    fix: json key on full name field

--- modules/user/api/model/req/create_user_req.go
+++ modules/user/api/model/req/create_user_req.go
@@ -1,7 +1,7 @@
 package req
 
 type CreateUserReq struct {
-	FullName string `json:"name" validate:"required"`
+	FullName string `json:"full_name" validate:"required"`
 	Email    string `json:"email" validate:"required,email"`
 	Phone    string `json:"phone" validate:"phone"`
 	Password string `json:"password" validate:"required,password"`
