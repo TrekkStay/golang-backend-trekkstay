aa75d684addcbc9d82c583a914e71eb74688c703
Phan Thanh An <thanhanphan17@gmail.com>
2024-01-22 23:30:33 +0700

    fix: missing file

---
+++ modules/user/domain/usecase/change_password_usecase.go
@@ -0,0 +1,79 @@
+package usecase
+
+import (
+	"context"
+	"log/slog"
+	"trekkstay/core"
+	"trekkstay/modules/user/constant"
+	"trekkstay/pkgs/log"
+)
+
+type ChangePasswordUseCase interface {
+	ExecChangePassword(ctx context.Context, oldPwd, newPwd string) error
+}
+
+type changePasswordUseCaseImpl struct {
+	hashAlgo   HashAlgo
+	readerRepo userReaderRepository
+	writerRepo userWriterRepository
+}
+
+var _ ChangePasswordUseCase = (*changePasswordUseCaseImpl)(nil)
+
+func NewChangePasswordUseCase(hashAlgo HashAlgo, readerRepo userReaderRepository,
+	writerRepo userWriterRepository) ChangePasswordUseCase {
+	return &changePasswordUseCaseImpl{
+		hashAlgo:   hashAlgo,
+		readerRepo: readerRepo,
+		writerRepo: writerRepo,
+	}
+}
+
+func (c changePasswordUseCaseImpl) ExecChangePassword(ctx context.Context, oldPwd, newPwd string) error {
+	// Find user by id
+	user, err := c.readerRepo.FindUserByCondition(ctx, map[string]interface{}{
+		"id": ctx.Value(core.CurrentRequesterKey).(core.Requester).GetUserId(),
+	})
+	if err != nil {
+		log.JsonLogger.Error("ExecChangePassword.find_user_by_id",
+			slog.String("error", err.Error()),
+			slog.String("request_id", ctx.Value("X-Request-ID").(string)),
+		)
+
+		return constant.ErrUserNotFound(err)
+	}
+
+	// Compare old password
+	if err := c.hashAlgo.ComparePasswords(user.Password, []byte(oldPwd)); err != nil {
+		log.JsonLogger.Error("ExecChangePassword.compare_password",
+			slog.String("error", err.Error()),
+			slog.String("request_id", ctx.Value("X-Request-ID").(string)),
+		)
+
+		return constant.ErrorWrongPassword(err)
+	}
+
+	// Hash new password
+	hashedPassword, err := c.hashAlgo.HashAndSalt([]byte(newPwd))
+	if err != nil {
+		log.JsonLogger.Error("ExecChangePassword.hash_password",
+			slog.String("error", err.Error()),
+			slog.String("request_id", ctx.Value("X-Request-ID").(string)),
+		)
+
+		return constant.ErrorHashPassword(err)
+	}
+	user.Password = hashedPassword
+
+	// Update user
+	if err := c.writerRepo.UpdateUser(ctx, *user); err != nil {
+		log.JsonLogger.Error("ExecChangePassword.update_user",
+			slog.String("error", err.Error()),
+			slog.String("request_id", ctx.Value("X-Request-ID").(string)),
+		)
+
+		return constant.ErrorInternalServerError(err)
+	}
+
+	return nil
+}
